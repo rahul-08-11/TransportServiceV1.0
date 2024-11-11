387d979543dc0109bb22ae90dc8f6f8f2683500c
rahul-08-11 <grahulkumar2002@gmail.com>
2024-11-11 14:41:23 +0530

    removed userreg/centralized

--- function_app.py
+++ function_app.py
@@ -11,9 +11,10 @@ from io import StringIO
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 carrierT = pd.read_csv("CarriersT.csv")
-@app.route(route="ping", methods=['GET'])
+@app.route(route="ping", methods=['GET','POST'])
 async def ping(req: func.HttpRequest) -> func.HttpResponse:
-    
+    logger.info(f'Request received from {req.url}')
+    logger.info(req.get_json())
     logger.info('Ping request received.')
     return func.HttpResponse("Service is up", status_code=200)
 
@@ -89,35 +90,3 @@ async def Quotations(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(json.dumps(body), status_code=200)
 
 
-
-
-@app.route(route="register-account",methods=['POST'])
-async def account_registration(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info(f'Request received from {req.url}')
-
-    try:
-        response =  await register_account(req=req)
-
-        logger.info(response)
-
-        return func.HttpResponse(json.dumps(response))
-  
-    except Exception as e:
-        logging.error(f'Error processing request: {str(e)}')
-        return func.HttpResponse(json.dumps({"error": str(e)}), status_code=500)
-    
-
-@app.route(route="register-carrier",methods=['POST'])
-async def carrier_registration(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info(f'Request received from {req.url}')
-
-    try:
-        response =  await register_carriers(req=req)
-
-        logger.info(response)
-
-        return func.HttpResponse(json.dumps(response))
-    
-    except Exception as e:
-        logging.error(f'Error processing request: {str(e)}')
-        return func.HttpResponse(json.dumps({"error": str(e)}), status_code=500)
--- src/apis/__init__.py
+++ src/apis/__init__.py
@@ -1,4 +1,3 @@
 from . import TJApi
 from . import CleadApi
 from .token_manager import TokenManager
-from .UserApi import add_customer_contact,add_bubble_carrier,link_contact_to_company_id,add_bubble_Customer,add_carrier_contact
